d785a2b79d3f15072479bac3483f4a4cb1acb92d
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-02-17 18:59:52 +0700

    Predefined return value

--- function/main.go
+++ function/main.go
@@ -10,8 +10,17 @@ func main() {
 	// fmt.Println("Keliling Persegi panjang = " ,keliling)
 
 	//tanda _ makaartinya tidak butuh/ tidak digunakan value ke 2
-	luas, _ := rectangle(10, 20)
-	fmt.Println("Luas Persegi panjang = " ,luas)
+	// luas, _ := rectangle(10, 20)
+	// fmt.Println("Luas rectangle = " ,luas)
+
+	
+	luas, kel := rectangle(10, 20)
+	fmt.Println("return value : ")
+	fmt.Println("Luas  = " , luas, "Keliling = " , kel)
+
+	luas2, kel2 := rectangle2(10, 20)
+	fmt.Println("Predefined return value : ")
+	fmt.Println("Luas  = " , luas2, "Keliling = " , kel2)
 	
 }
 
@@ -25,6 +34,14 @@ func rectangle(panjang, lebar int) (int, int) {
 	keliling := 2 * (panjang + lebar)
 	
 	return luas, keliling
+}
+
+//Predefined return value
+func rectangle2(panjang, lebar int) (luas int, keliling int) {
+	luas = panjang * lebar
+	keliling = 2 * (panjang + lebar)
+	
+	return 
 }
 
 //note: nama func di awalai kapital maka publik sedangakan huruf kecil maka private
